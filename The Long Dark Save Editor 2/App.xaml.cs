3ee2a5caf33f6bfe6d498d1a2c81c79145adc63f
FINDarkside <FINDarkside@gmail.com>
2017-02-13 16:53:36 +0200

    Added crashlog

--- The Long Dark Save Editor 2/App.xaml.cs
+++ The Long Dark Save Editor 2/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
 
 namespace The_Long_Dark_Save_Editor_2
 {
@@ -7,5 +10,20 @@ namespace The_Long_Dark_Save_Editor_2
 	/// </summary>
 	public partial class App : Application
 	{
+		public App() : base()
+		{
+			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler((object sender, UnhandledExceptionEventArgs args) =>
+			{
+				string codeBase = System.Reflection.Assembly.GetExecutingAssembly().Location;
+				UriBuilder uri = new UriBuilder(codeBase);
+				string path = Uri.UnescapeDataString(uri.Path);
+				path = Path.Combine(Path.GetDirectoryName(path), "crash.txt");
+
+				File.WriteAllText(path, args.ExceptionObject.ToString());
+
+				Environment.Exit(-1);
+			});
+		}
+
 	}
 }
