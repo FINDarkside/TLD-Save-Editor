0b1b3bcea60c9efca19254f66a5665decd8fd356
FINDarkside <findarkside@gmail.com>
2017-06-28 17:57:18 +0300

    Clean EncryptString

--- The Long Dark Save Editor 2/Helpers/EncryptString.cs
+++ The Long Dark Save Editor 2/Helpers/EncryptString.cs
@@ -5,75 +5,23 @@ using System.Security.Cryptography;
 
 namespace The_Long_Dark_Save_Editor_2.Helpers
 {
-	public class EncryptString
-	{
-		private static bool m_Initialized;
-		private static byte[] keyArray;
-		private static ICryptoTransform m_EncoderTransform;
-		private static ICryptoTransform m_DecoderTransform;
+    public class EncryptString
+    {
+        public static byte[] CompressStringToBytes(string toEncrypt)
+        {
+            return CLZF.Compress(Encoding.UTF8.GetBytes(toEncrypt));
+        }
 
-		static EncryptString()
-		{
-			EncryptString.keyArray = Encoding.UTF8.GetBytes(EncryptString.GetKey());
-			RijndaelManaged rijndaelManaged = new RijndaelManaged();
-			rijndaelManaged.Key = EncryptString.keyArray;
-			rijndaelManaged.Mode = CipherMode.ECB;
-			rijndaelManaged.Padding = PaddingMode.PKCS7;
-			EncryptString.m_EncoderTransform = rijndaelManaged.CreateEncryptor();
-			EncryptString.m_DecoderTransform = rijndaelManaged.CreateDecryptor();
-			EncryptString.m_Initialized = true;
-
-		}
-
-		private static string GetKey()
-		{
-			return "FL8j2zG0wY9MkUp9DJlXsW8f9FUaZ9nL";
-		}
-
-		public static string Encrypt(string toEncrypt)
-		{
-			byte[] bytes = Encoding.UTF8.GetBytes(toEncrypt);
-			byte[] inArray = EncryptString.m_EncoderTransform.TransformFinalBlock(bytes, 0, bytes.Length);
-			return Convert.ToBase64String(inArray, 0, inArray.Length);
-		}
-
-		public static string Decrypt(string toDecrypt)
-		{
-			try
-			{
-				byte[] inputBuffer = Convert.FromBase64String(toDecrypt);
-				return Encoding.UTF8.GetString(EncryptString.m_DecoderTransform.TransformFinalBlock(inputBuffer, 0, inputBuffer.Length));
-			}
-			catch
-			{
-				return string.Empty;
-			}
-		}
-
-		public static byte[] CompressStringToBytes(string toEncrypt)
-		{
-			return CLZF.Compress(Encoding.UTF8.GetBytes(toEncrypt));
-		}
-
-		public static string DecompressBytesToString(byte[] toDecrypt)
-		{
-			try
-			{
-				string @string = Encoding.UTF8.GetString(CLZF.Decompress(toDecrypt));
-				if (!string.IsNullOrEmpty(@string))
-				{
-					if (@string.StartsWith("{"))
-					{
-						if (@string.EndsWith("}"))
-							return @string;
-					}
-				}
-			}
-			catch
-			{
-				return string.Empty;
-			}
-			return string.Empty;
-		}
-	}
+        public static string DecompressBytesToString(byte[] toDecrypt)
+        {
+            try
+            {
+                return Encoding.UTF8.GetString(CLZF.Decompress(toDecrypt));
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
 }
