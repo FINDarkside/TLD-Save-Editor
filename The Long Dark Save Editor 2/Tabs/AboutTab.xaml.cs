6dc4987e876282116263134ffa71454a4c8035e1
FINDarkside <findarkside@gmail.com>
2018-04-14 19:19:14 +0300

    Add nbernier as contributor, add GitHub repo link to About tab

--- The Long Dark Save Editor 2/Tabs/AboutTab.xaml.cs
+++ The Long Dark Save Editor 2/Tabs/AboutTab.xaml.cs
@@ -1,15 +1,19 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace The_Long_Dark_Save_Editor_2.Tabs
 {
-    /// <summary>
-    /// Interaction logic for AboutTab.xaml
-    /// </summary>
     public partial class AboutTab : UserControl
     {
         public AboutTab()
         {
             InitializeComponent();
+        }
+
+        private void ViewOnGithubClicked(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/FINDarkside/TLD-Save-Editor");
         }
     }
 }
