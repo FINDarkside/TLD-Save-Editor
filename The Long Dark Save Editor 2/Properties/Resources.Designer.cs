78e00da5e121c1392e7dde2dd0cb1b4fe0a3d4a1
FINDarkside <findarkside@gmail.com>
2018-04-14 19:21:33 +0300

    Update version to 1.10.0

--- The Long Dark Save Editor 2/Properties/Resources.Designer.cs
+++ The Long Dark Save Editor 2/Properties/Resources.Designer.cs
@@ -3823,7 +3823,7 @@ namespace The_Long_Dark_Save_Editor_2.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The Long Dark (v1.21) Save Editor.
+        ///   Looks up a localized string similar to The Long Dark (v1.29) Save Editor.
         /// </summary>
         public static string Title {
             get {
