7dd999645cad009907af45f47bb801a87f5f5afa
FINDarkside <findarkside@gmail.com>
2019-04-05 15:36:48 +0300

    Implement serializing DynamicSerializable

--- The Long Dark Save Editor 2/Helpers/DynamicSerializable.cs
+++ The Long Dark Save Editor 2/Helpers/DynamicSerializable.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Dynamic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -13,7 +15,7 @@ namespace The_Long_Dark_Save_Editor_2.Helpers
     public class DynamicSerializable<T> where T : new()
     {
 
-        private Dictionary<object, JToken> extraFields = new Dictionary<object, JToken>();
+        private Dictionary<object, List<KeyValuePair<string, JToken>>> extraFields = new Dictionary<object, List<KeyValuePair<string, JToken>>>();
         public T Obj { get; private set; }
 
         public DynamicSerializable(string json)
@@ -52,6 +54,78 @@ namespace The_Long_Dark_Save_Editor_2.Helpers
             }
         }
 
+        public string Serialize()
+        {
+            var res = ReconstructObject(Obj);
+            return JsonConvert.SerializeObject(res);
+        }
+
+        public object Reconstruct(object o, DeserializeAttribute attr = null)
+        {
+            object result = null;
+            if (o == null)
+                result = null;
+            else
+            {
+                Type t = o.GetType();
+                if (IsBoxed(o) || o is string)
+                {
+                    result = o;
+                }
+                else if (o is ICollection)
+                {
+                    result = ReconstructCollection((ICollection)o);
+                }
+                else
+                {
+                    result = ReconstructObject(o);
+                }
+            }
+
+            if (attr != null && attr.Json)
+                return JsonConvert.SerializeObject(result);
+            return result;
+        }
+
+        public object ReconstructObject(object o)
+        {
+            var res = new Dictionary<string, dynamic>();
+            var t = o.GetType();
+            PropertyInfo[] props = t.GetProperties().Where(p => p.GetCustomAttribute<NonSerializedAttribute>() == null).ToArray();
+            FieldInfo[] fields = t.GetFields().Where(p => p.GetCustomAttribute<NonSerializedAttribute>() == null).ToArray();
+
+            foreach (var prop in props)
+            {
+                var attr = prop.GetCustomAttribute<DeserializeAttribute>();
+                var name = attr?.From ?? prop.Name;
+                res.Add(name, Reconstruct(prop.GetValue(o), attr));
+            }
+            foreach (var field in fields)
+            {
+                var attr = field.GetCustomAttribute<DeserializeAttribute>();
+                var name = attr?.From ?? field.Name;
+                res.Add(name, Reconstruct(field.GetValue(o), attr));
+            }
+            if (extraFields.ContainsKey(o))
+            {
+                foreach (var kvp in extraFields[o])
+                {
+                    res.Add(kvp.Key, kvp.Value);
+                }
+            }
+            return res;
+        }
+
+        public List<object> ReconstructCollection(ICollection col)
+        {
+            List<object> result = new List<object>();
+            foreach (var item in col)
+            {
+                result.Add(Reconstruct(item));
+            }
+            return result;
+        }
+
         private object ParseObject(JObject obj, Type t)
         {
             var result = Activator.CreateInstance(t);
@@ -78,7 +152,9 @@ namespace The_Long_Dark_Save_Editor_2.Helpers
                 }
                 else
                 {
-                    extraFields.Add(result, child.Value);
+                    if (!extraFields.ContainsKey(result))
+                        extraFields.Add(result, new List<KeyValuePair<string, JToken>>());
+                    extraFields[result].Add(child);
                 }
             }
             return result;
@@ -102,5 +178,14 @@ namespace The_Long_Dark_Save_Editor_2.Helpers
                 return attr.From;
             return m.Name;
         }
+
+        public static bool IsBoxed<T>(T value)
+        {
+            return
+                (typeof(T).IsInterface || typeof(T) == typeof(object)) &&
+                value != null &&
+                value.GetType().IsValueType;
+        }
+
     }
 }
