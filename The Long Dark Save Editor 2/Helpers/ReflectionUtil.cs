e167f2d623ec39f6be799a2062b63b8404be6ade
FINDarkside <findarkside@gmail.com>
2019-04-05 21:05:27 +0300

    Add ReflectionUtil

---
+++ The Long Dark Save Editor 2/Helpers/ReflectionUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Long_Dark_Save_Editor_2.Helpers
+{
+    public static class ReflectionUtil
+    {
+        public static object ConvertArray(Array arr, Type t)
+        {
+            if (t.IsArray)
+            {
+                return Convert.ChangeType(arr, t);
+            }else if((typeof(IList).IsAssignableFrom(t)))
+            {
+                var list = (IList)Activator.CreateInstance(t);
+                foreach(var item in arr)
+                {
+                    list.Add(item);
+                }
+                return list;
+            }
+            throw new Exception();
+        }
+    }
+}
