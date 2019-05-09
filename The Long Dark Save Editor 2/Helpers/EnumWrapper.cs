1621daa7eb9b258b158e7bff943ce7f5564e0792
FINDarkside <findarkside@gmail.com>
2019-05-09 20:53:55 +0300

    Add EnumWrapper

---
+++ The Long Dark Save Editor 2/Helpers/EnumWrapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace The_Long_Dark_Save_Editor_2.Helpers
+{
+    public class EnumWrapper<T> : INotifyPropertyChanged where T : Enum
+    {
+        static class EnumValues<T> where T : Enum
+        {
+            public static List<string> values = Enum.GetNames(typeof(T)).ToList();
+        }
+
+        private string currentValue;
+        public string CurrentValue
+        {
+            get { return CurrentValue; }
+            set
+            {
+                if (!EnumValues<T>.values.Contains(value)){
+                    EnumValues<T>.values.Add(value);
+                }
+                SetPropertyField(ref currentValue, value);
+            }
+        }
+
+        public List<string> GetValues()
+        {
+            return EnumValues<T>.values;
+        }
+
+        protected void SetPropertyField<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
