a763716c42d14b57bd957c745980a0a5d4ce2e02
FINDarkside <findarkside@gmail.com>
2019-10-27 22:04:42 +0200

    2.15

--- The Long Dark Save Editor 2/Properties/Resources.Designer.cs
+++ The Long Dark Save Editor 2/Properties/Resources.Designer.cs
@@ -4399,7 +4399,7 @@ namespace The_Long_Dark_Save_Editor_2.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The Long Dark (v1.50) Save Editor.
+        ///   Looks up a localized string similar to The Long Dark (v1.60) Save Editor.
         /// </summary>
         public static string Title {
             get {
