96620edc4834d92b9eea5b842d08a43d834a2d06
FINDarkside <findarkside@gmail.com>
2019-11-12 06:50:16 +0200

    Refactor GetCustomAttributeCached

--- The Long Dark Save Editor 2/Serialization/CachedReflection.cs
+++ The Long Dark Save Editor 2/Serialization/CachedReflection.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Reflection;
 
 namespace The_Long_Dark_Save_Editor_2.Serialization
 {
-    static class PropertyCustomAttributeCache<T>
-    {
-        public static Dictionary<PropertyInfo, T> cache = new Dictionary<PropertyInfo, T>();
-        public static Dictionary<PropertyInfo, T> cacheNoInherit = new Dictionary<PropertyInfo, T>();
-    }
     static class MemberCustomAttributeCache<T>
     {
         public static Dictionary<MemberInfo, T> cache = new Dictionary<MemberInfo, T>();
@@ -18,18 +12,6 @@ namespace The_Long_Dark_Save_Editor_2.Serialization
 
     static class CachedReflection
     {
-
-        public static T GetCustomAttributeCached<T>(this PropertyInfo propInfo, bool inherit) where T : Attribute
-        {
-            var cache = inherit ? PropertyCustomAttributeCache<T>.cache : PropertyCustomAttributeCache<T>.cacheNoInherit;
-            if (cache.ContainsKey(propInfo))
-            {
-                return cache[propInfo];
-            }
-            var attr = propInfo.GetCustomAttribute<T>(false);
-            cache.Add(propInfo, attr);
-            return attr;
-        }
 
         public static T GetCustomAttributeCached<T>(this MemberInfo memberInfo, bool inherit) where T : Attribute
         {
