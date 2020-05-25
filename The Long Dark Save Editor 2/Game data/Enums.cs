c99c187b6e8db1732ef7fce6d8dc8b2582e3b614
Pt-Djefferson <alex.tesla@gmail.com>
2020-05-25 22:10:11 +0900

    fix for issue #49

--- The Long Dark Save Editor 2/Game data/Enums.cs
+++ The Long Dark Save Editor 2/Game data/Enums.cs
@@ -323,6 +323,7 @@ namespace The_Long_Dark_Save_Editor_2.Game_data
         MountainTownRegion,
         TracksRegion,
         RiverValleyRegion,
+        CanneryRegion,
     }
 
     public enum UpSell
